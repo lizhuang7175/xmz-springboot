3b3c616f83372a3e78b662e263bfedb3ec237971
lizhuang <lizhuang7175@outlook.com>
2024-10-15 17:48:51 +0800

    init

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # xmz-springboot
-一套基于SpringBoot的开发框架，整合了JWT，Redis，Mybatis-Plus（包含其后端代码生成工具），MongoBD文件管理等等，整合了一些常用的工具类
+一套基于SpringBoot 2.5.0和JDK1.8的开发框架，整合了JWT，Redis，Mybatis-Plus（包含其后端代码生成工具），MongoBD文件管理等等，整合了一些常用的工具类
---
+++ src/main/java/com/XmzApplication.java
@@ -0,0 +1,21 @@
+package com;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+@MapperScan({"com.**.mapper"})  //mapper包扫描
+@SpringBootApplication
+public class XmzApplication extends SpringBootServletInitializer {
+
+    public static void main(String[] args) {
+        SpringApplication.run(XmzApplication.class, args);
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(XmzApplication.class);
+    }
+}
